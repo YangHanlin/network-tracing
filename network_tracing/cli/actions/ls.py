dda76ac9ed0cff32b03a816131ebcf899fc3203a
Yang Hanlin <mattoncis@hotmail.com>
2023-02-11 13:11:36 +0800

    feat(cli): implement subcommand `ls`

--- network_tracing/cli/actions/ls.py
+++ network_tracing/cli/actions/ls.py
@@ -1,4 +1,11 @@
+import logging
+import sys
 from argparse import _SubParsersAction
+
+from network_tracing.cli.api import ApiClient
+from network_tracing.cli.constants import DEFAULT_PROGRAM_NAME
+
+logger = logging.getLogger(__name__)
 
 
 def configure_subparsers(subparsers: _SubParsersAction):
@@ -7,5 +14,19 @@ def configure_subparsers(subparsers: _SubParsersAction):
                                    help='list all tracing tasks')
 
 
-def run(options: dict) -> None:
-    pass
+def run(_=None):
+    try:
+        tracing_tasks = ApiClient.get_instance().list_tracing_tasks()
+        print('{:32} {}'.format('ID', 'PROBES'))
+        for task in tracing_tasks:
+            probes = task.options.probes.keys()
+            print('{:32} {} ({})'.format(task.id, ', '.join(probes),
+                                         len(probes)))
+    except Exception as e:
+        logger.debug('Encountered an exception', exc_info=e)
+        print('{}: error: failed to list tracing tasks: {}'.format(
+            DEFAULT_PROGRAM_NAME,
+            e,
+        ),
+              file=sys.stderr)
+        return 1

66ce9ca401eb7c7520b470f3c133d61c524b0886
Yang Hanlin <mattoncis@hotmail.com>
2023-02-11 13:30:20 +0800

    feat(cli): implement subcommand `stop`

--- network_tracing/cli/actions/stop.py
+++ network_tracing/cli/actions/stop.py
@@ -1,4 +1,15 @@
-from argparse import _SubParsersAction, ArgumentParser
+import sys
+from argparse import ArgumentParser, _SubParsersAction
+from dataclasses import dataclass
+
+from network_tracing.cli.api import ApiClient, ApiException
+from network_tracing.cli.constants import DEFAULT_PROGRAM_NAME
+from network_tracing.cli.models import BaseOptions
+
+
+@dataclass
+class Options(BaseOptions):
+    id: str
 
 
 def configure_subparsers(subparsers: _SubParsersAction):
@@ -12,5 +23,15 @@ def configure_subparsers(subparsers: _SubParsersAction):
                         help='ID of tracing task to stop and remove')
 
 
-def run(options: dict) -> None:
-    pass
+def run(options_dict: dict):
+    options = Options.from_dict(options_dict)
+
+    try:
+        ApiClient.get_instance().remove_tracing_task(options.id)
+    except ApiException as e:
+        print('{}: error: failed to stop and remove task: {}'.format(
+            DEFAULT_PROGRAM_NAME,
+            e,
+        ),
+              file=sys.stderr)
+        return 1
